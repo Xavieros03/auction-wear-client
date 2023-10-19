f088fbd7b021d4513e7d1d245bf7c84f96459a76
Xavier Ortega <xaviersendra@hotmail.com>
2023-10-19 20:20:54 +0200

    test3

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,45 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import AuctionList from '../components/AuctionList';
-import io from 'socket.io-client'; 
+import io from 'socket.io-client';
+import { Link, NavLink } from 'react-router-dom';
 
-const socket = io.connect('wss://auction-server-project.onrender.com'); 
+const socket = io.connect('wss://auction-server-project.onrender.com');
 
 function MainPage() {
     const [authenticated, setAuthenticated] = useState(false);
-    const [auctions, setAuctions] = useState([]); 
+    const [auctions, setAuctions] = useState([]);
     useEffect(() => {
         const userToken = localStorage.getItem('token');
         if (userToken) {
             setAuthenticated(true);
         }
 
-       
+
         socket.on('auctionCreatedOrUpdated', (updatedAuction) => {
-          
+
             setAuctions((prevAuctions) => {
-               
+
                 const existingAuctionIndex = prevAuctions.findIndex(
                     (auction) => auction._id === updatedAuction._id
                 );
 
                 if (existingAuctionIndex !== -1) {
-                  
+
                     prevAuctions[existingAuctionIndex] = updatedAuction;
                     return [...prevAuctions];
                 } else {
-                   
+
                     return [...prevAuctions, updatedAuction];
                 }
             });
         });
 
-       
+
         socket.on('productUpdated', (updatedProduct) => {
-          
+
             setAuctions((prevAuctions) => {
                 return prevAuctions.map((auction) => {
                     if (auction.product && auction.product._id === updatedProduct._id) {
-                       
+
                         return {
                             ...auction,
                             product: updatedProduct,
@@ -62,8 +63,8 @@ function MainPage() {
             {authenticated ? (
                 <>
                     <p>This is the main page content for authenticated users.</p>
-                    <a href="/products/all">Products</a>
-                    <AuctionList auctions={auctions} /> 
+                    <Link to="/products/all">Products</Link>
+                    <AuctionList auctions={auctions} />
                 </>
             ) : (
                 <p>You need to log in to access this content.</p>
